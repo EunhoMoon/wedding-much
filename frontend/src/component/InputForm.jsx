f29dd84c1810f16623f63e8a056f8b3c3ecac17a
Janek <eunho.moon@gmail.com>
2023-09-12 23:26:47 +0900

    [fix] input 수정

--- frontend/src/component/InputForm.jsx
+++ frontend/src/component/InputForm.jsx
@@ -1,41 +1,36 @@
 import React, {useRef, useState} from 'react';
 import {Button, TextField} from "@mui/material";
 
-const InputForm = props => {
-  const nameRef = useRef('');
-  const priceRef = useRef(0);
-  const memoRef = useRef('');
+const initGift = {
+  name: '',
+  price: '',
+  memo: ''
+};
 
-  const [newGift, setNewGift] = useState({
-    name: '',
-    price: '',
-    memo: ''
-  });
+const InputForm = props => {
+  const [newGift, setNewGift] = useState(initGift);
 
   const setProperties = (e) => {
-    setNewGift({...newGift, [e.target.name] : e.target.value});
+    setNewGift({...newGift, [e.target.name]: e.target.value});
   }
 
   const submitHandler = e => {
     e.preventDefault();
     props.onGiftSave(newGift);
-    setNewGift({
-      name: '',
-      price: '',
-      memo: ''
-    })
+    setNewGift(initGift)
   }
 
   return (
     <div style={{display: 'flex', justifyContent: 'flex-start'}}>
-        <TextField style={{margin: '0 5px'}} name="name" label="이름" variant="outlined" size="small" color="success" value={newGift.name} onChange={setProperties}
-                   ref={nameRef}/>
-        <TextField style={{margin: '0 5px'}} name="price" label="금액" variant="outlined" size="small" color="success" value={newGift.price} onChange={setProperties}
-                   type="number" ref={priceRef}/>
-        <TextField style={{margin: '0 5px'}} name="memo" label="비고" variant="outlined" size="small" color="success" value={newGift.memo} onChange={setProperties}
-                   ref={memoRef}/>
-        <Button type="submit" variant="outlined" style={{margin: '0 5px'}} color="success"
-                onClick={submitHandler}>저장</Button>
+      <input type="text"/>
+      <TextField style={{margin: '0 5px'}} name="name" label="이름" variant="outlined" size="small" color="success"
+                 value={newGift.name} onChange={setProperties}/>
+      <TextField style={{margin: '0 5px'}} name="price" label="금액" variant="outlined" size="small" color="success"
+                 value={newGift.price} onChange={setProperties}/>
+      <TextField style={{margin: '0 5px'}} name="memo" label="비고" variant="outlined" size="small" color="success"
+                 value={newGift.memo} onChange={setProperties}/>
+      <Button type="submit" variant="outlined" style={{margin: '0 5px'}} color="success"
+              onClick={submitHandler}>저장</Button>
     </div>
   );
 };
