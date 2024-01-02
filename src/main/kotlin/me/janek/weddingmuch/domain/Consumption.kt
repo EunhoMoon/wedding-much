0e0aae4ffce03ba26aec11d8118d72a0d89c6853
Janek <eunho.moon@gmail.com>
2024-01-02 22:41:54 +0900

    소비 도메인 추가

---
+++ src/main/kotlin/me/janek/weddingmuch/domain/Consumption.kt
@@ -0,0 +1,24 @@
+package me.janek.weddingmuch.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+
+@Entity
+@Table(name = "consumptions")
+class Consumption(
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  val id: Long? = 0,
+
+  @Column(nullable = false)
+  val token: String,
+
+  @Column(nullable = false)
+  val place: String,
+) {
+}
