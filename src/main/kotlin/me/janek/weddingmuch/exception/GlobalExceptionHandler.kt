3dfc5eb4a09d8bc540b77f7269b883b7f2aa5f13
Janek <eunho.moon@gmail.com>
2024-01-03 01:24:36 +0900

    [fix] Add exception layer

--- src/main/kotlin/me/janek/weddingmuch/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/me/janek/weddingmuch/exception/GlobalExceptionHandler.kt
@@ -1,4 +1,4 @@
-package me.janek.weddingmuch
+package me.janek.weddingmuch.exception
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.MethodArgumentNotValidException
