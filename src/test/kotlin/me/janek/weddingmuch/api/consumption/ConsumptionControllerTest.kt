083ea1a9218530873a1ffd173d3c09d39dd7ef7e
Janek <eunho.moon@gmail.com>
2024-11-23 18:11:29 +0900

    [fix] fix bugs from remove spring rest docs

--- src/test/kotlin/me/janek/weddingmuch/api/consumption/ConsumptionControllerTest.kt
+++ src/test/kotlin/me/janek/weddingmuch/api/consumption/ConsumptionControllerTest.kt
@@ -8,14 +8,13 @@ import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.*
 
 @AutoConfigureMockMvc(addFilters = false)
-//@WebMvcTest(controllers = [ConsumptionController::class])
-@SpringBootTest
+@WebMvcTest(controllers = [ConsumptionController::class])
 class ConsumptionControllerTest @Autowired constructor(
   private val objectMapper: ObjectMapper,
   private var mockMvc: MockMvc,
